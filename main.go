80f595ee71deda80f4cbaaac225aeaec39dda0aa
Adit <adhityapmp@gmail.com>
2026-03-14 18:17:39 +0700

    ci: implement auto-migrate on startup and simplify deploy workflow to match dev branch

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"os"
 	"rextra-backend/cmd"
+	"rextra-backend/db"
+	"rextra-backend/db/migrations"
+	seeders "rextra-backend/db/seeder"
 	"rextra-backend/internal/config"
+	mylog "rextra-backend/internal/pkg/logger"
 
 	"github.com/joho/godotenv"
 )
@@ -10,12 +15,23 @@ import (
 const ENV_FILE = ".env.dev"
 
 func main() {
-	if err := godotenv.Load(ENV_FILE); err != nil {
-		panic("Failed to loading env file")
+	_ = godotenv.Load(ENV_FILE)
+
+	// Jika dipanggil dengan flag (CLI mode)
+	if len(os.Args) > 1 {
+		if err := cmd.Commands(); err != nil {
+			panic("Failed Get Commands: " + err.Error())
+		}
+		return
 	}
 
-	if err := cmd.Commands(); err != nil {
-		panic("Failed Get Commands: " + err.Error())
+	// Mode API Server: Jalankan auto-migrate & seeder di Dev Mode
+	if os.Getenv("APP_MODE") == "development" {
+		database := db.New()
+		mylog.Infof("[AUTO] Running auto migration...")
+		_ = migrations.Migrate(database)
+		mylog.Infof("[AUTO] Running auto seeder...")
+		_ = seeders.Seeding(database)
 	}
 
 	RestApi := config.NewRest()
